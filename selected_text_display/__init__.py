1015f4c5f05ce885c9b692fefe278cae3bad0ecb
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-11-10 22:22:25 -0800

    Update __init__.py

--- selected_text_display/__init__.py
+++ selected_text_display/__init__.py
@@ -36,7 +36,8 @@ if 'words' not in st.session_state:
 if "txt" not in st.session_state:
     st.session_state.txt = ""
 def main():
-    st.write(f"Screen width is {streamlit_js_eval(js_expressions='screen.width', key = 'SCR')}")
+    screen_width = {streamlit_js_eval(js_expressions='screen.width', key = 'SCR')}
+    # st.write(f"Screen width is {streamlit_js_eval(js_expressions='screen.width', key = 'SCR')}")
     m = st.markdown("""<style> div.stButton > button:first-child {
   background-color: #4e83e0; /* Background color */
   color: #ffffff; /* Text color */
@@ -51,84 +52,146 @@ def main():
     height:2.em;width:50%;
 }
 </style>""", unsafe_allow_html=True)
-    col1, col2 = st.columns([2,1])
-    with col1:
-        st.title("English to ASL")
-        st.subheader("Put in your paragraph here: ")
-        on = st.toggle('Choose here to use picture')
-        if not on:
-            st.session_state.txt = st.text_area("Input")
-        else:
-            image_file = st.file_uploader("Upload Image",type=['jpg','png','jpeg','JPG'])
+    if screen_width < 400:
+      on_phone()
+    else:
+      col1, col2 = st.columns([2,1])
+      with col1:
+          st.title("English to ASL")
+          st.subheader("Put in your paragraph here: ")
+          on = st.toggle('Choose here to use picture')
+          if not on:
+              st.session_state.txt = st.text_area("Input")
+          else:
+              image_file = st.file_uploader("Upload Image",type=['jpg','png','jpeg','JPG'])
+      
+              if image_file is not None:
+                  img = Image.open(image_file)
+                  img = np.array(img)
+                  
+                  st.subheader('Image you Uploaded...')
+                  st.image(image_file,width=450)
+                  if st.session_state.img == False:
+                      with st.spinner('Extracting Text from given Image'):
+                          eng_reader = easyocr.Reader(['en'])
+                          detected_text = eng_reader.readtext(img)
+                      st.session_state.txt = display_text(detected_text)
+                      st.session_state.img = True
+      with col2:
+          st.image('./video-recording_V1.gif')
+      with col1:
+          st.divider()
+          st.subheader("Choose your words to translate here by highlighting the word and clicking 'Select Text'")
+      with col2:
+          st.image('./video-recording_V1.gif')
+      with col1:
+          selected_text = selected_text_display(st.session_state.txt)
+          
+          if selected_text not in st.session_state.words and selected_text not in [None, '', ' ']:
+              st.session_state.words.append(selected_text)
+      container_dict = {}
+      left_column_dict = {}
+      right_column_dict = {}
+  
+      if st.button("Translate to ASL"):
+          syns = disambiguate(st.session_state.txt)
+          result_dict = dict(syns)
+  
+          for word in st.session_state.words:
+              container_dict[word] = st.container()
+              left_column_dict[word], right_column_dict[word] = st.columns([2,1])
+          for word in st.session_state.words:
+              video_links = find_words_asl(word)
+              if video_links != []:
+                  with container_dict[word]:
+                      with left_column_dict[word]:
+                          st.subheader(word)
+                          for video in video_links:
+                              st.video(video)
+                      with right_column_dict[word]:
+                          st.subheader("Root: ")
+                          _, pos, _ = result_dict[word].name().split('.')
+                          st.write(wnl.lemmatize(word, pos=pos))
+                          st.subheader("This English word can also mean: ")
+                          lstsyn = find_synonyms(word, syns)
+                          for syn in lstsyn:
+                              st.write(syn)
+              else:
+                  lstsyn = find_synonyms(word, syns)
+                  for syn in lstsyn:
+                      video_links = find_words_asl(syn)
+                      if video_links != []:
+                          st.write(syn)
+                          break
+                  for video in video_links:
+                      st.video(video)
+  
+  
+      if st.button("Restart"):
+          st.session_state.words = []
+          st.session_state.img = False
+          st.session_state.txt = ""
+          streamlit_js_eval(js_expressions="parent.window.location.reload()")
+
+def on_phone():
+          st.title("English to ASL")
+          st.subheader("Put in your paragraph here: ")
+          on = st.toggle('Choose here to use picture')
+          if not on:
+              st.session_state.txt = st.text_area("Input")
+          else:
+              image_file = st.file_uploader("Upload Image",type=['jpg','png','jpeg','JPG'])
+      
+              if image_file is not None:
+                  img = Image.open(image_file)
+                  img = np.array(img)
+                  
+                  st.subheader('Image you Uploaded...')
+                  st.image(image_file,width=450)
+                  if st.session_state.img == False:
+                      with st.spinner('Extracting Text from given Image'):
+                          eng_reader = easyocr.Reader(['en'])
+                          detected_text = eng_reader.readtext(img)
+                      st.session_state.txt = display_text(detected_text)
+                      st.session_state.img = True
+          st.divider()
+          st.subheader("Choose your words to translate here by highlighting the word and clicking 'Select Text'")
+          selected_text = selected_text_display(st.session_state.txt)
+          
+          if selected_text not in st.session_state.words and selected_text not in [None, '', ' ']:
+              st.session_state.words.append(selected_text)
+        container_dict = {}
+  
+        if st.button("Translate to ASL"):
+            syns = disambiguate(st.session_state.txt)
+            result_dict = dict(syns)
     
-            if image_file is not None:
-                img = Image.open(image_file)
-                img = np.array(img)
-                
-                st.subheader('Image you Uploaded...')
-                st.image(image_file,width=450)
-                if st.session_state.img == False:
-                    with st.spinner('Extracting Text from given Image'):
-                        eng_reader = easyocr.Reader(['en'])
-                        detected_text = eng_reader.readtext(img)
-                    st.session_state.txt = display_text(detected_text)
-                    st.session_state.img = True
-    with col2:
-        st.image('./video-recording_V1.gif')
-    with col1:
-        st.divider()
-        st.subheader("Choose your words to translate here by highlighting the word and clicking 'Select Text'")
-    with col2:
-        st.image('./video-recording_V1.gif')
-    with col1:
-        selected_text = selected_text_display(st.session_state.txt)
-        
-        if selected_text not in st.session_state.words and selected_text not in [None, '', ' ']:
-            st.session_state.words.append(selected_text)
-    container_dict = {}
-    left_column_dict = {}
-    right_column_dict = {}
+            for word in st.session_state.words:
+                container_dict[word] = st.container()
 
-    if st.button("Translate to ASL"):
-        syns = disambiguate(st.session_state.txt)
-        result_dict = dict(syns)
-
-        for word in st.session_state.words:
-            container_dict[word] = st.container()
-            left_column_dict[word], right_column_dict[word] = st.columns([2,1])
-        for word in st.session_state.words:
-            video_links = find_words_asl(word)
-            if video_links != []:
-                with container_dict[word]:
-                    with left_column_dict[word]:
-                        st.subheader(word)
-                        for video in video_links:
-                            st.video(video)
-                    with right_column_dict[word]:
-                        st.subheader("Root: ")
-                        _, pos, _ = result_dict[word].name().split('.')
-                        st.write(wnl.lemmatize(word, pos=pos))
-                        st.subheader("This English word can also mean: ")
-                        lstsyn = find_synonyms(word, syns)
-                        for syn in lstsyn:
+            for word in st.session_state.words:
+                video_links = find_words_asl(word)
+                if video_links != []:
+                    with container_dict[word]:
+                            st.subheader(word)
+                            for video in video_links:
+                                st.video(video)
+                else:
+                    lstsyn = find_synonyms(word, syns)
+                    for syn in lstsyn:
+                        video_links = find_words_asl(syn)
+                        if video_links != []:
                             st.write(syn)
-            else:
-                lstsyn = find_synonyms(word, syns)
-                for syn in lstsyn:
-                    video_links = find_words_asl(syn)
-                    if video_links != []:
-                        st.write(syn)
-                        break
-                for video in video_links:
-                    st.video(video)
-
-
-    if st.button("Restart"):
-        st.session_state.words = []
-        st.session_state.img = False
-        st.session_state.txt = ""
-        streamlit_js_eval(js_expressions="parent.window.location.reload()")
-
+                            break
+                    for video in video_links:
+                        st.video(video)
+    
+    
+        if st.button("Restart"):
+            st.session_state.words = []
+            st.session_state.img = False
+            st.session_state.txt = ""
+            streamlit_js_eval(js_expressions="parent.window.location.reload()")
         
 def find_words_asl(word):
     word = word.lower()
