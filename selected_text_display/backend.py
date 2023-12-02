61ff746e81a6e4478afe4a55e9d5538b1409f4cc
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-12-02 15:18:12 -0800

    Update backend.py

--- selected_text_display/backend.py
+++ selected_text_display/backend.py
@@ -30,7 +30,7 @@ def english_root_and_synonyms(synsets, word):
       _, pos, _ = synsets[word].name().split('.')
       root = wnl.lemmatize(word, pos=pos)
       lstsyn = find_synonyms(word, synsets)
-      asl_synonyms = result.loc[result['word'] == word]['synonyms']
+      asl_synonyms = result.loc[result['word'] == root]['synonyms']
       return root, lstsyn, asl_synonyms
 
 def find_words_asl(word, synsets):
